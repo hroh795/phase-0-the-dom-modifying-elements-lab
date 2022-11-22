079d77f73b6b43c84fc9a24582b56a99512c589c
Ha Young Roh <hyroh795@gmail.com>
2022-11-22 16:01:45 -0500

    commit tree 5d9b980ad34828765f29a87df9d0c127b3f58227

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");
+
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = "Ha Young is the champion";
